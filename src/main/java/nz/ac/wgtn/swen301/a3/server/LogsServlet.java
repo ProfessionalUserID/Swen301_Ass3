ad3bd8387aed72a03071f8b97c0fe9ab10971e32
Joshua <cookhjosh@ecs.vuw.ac.nz>
2021-05-25 15:43:01 +1200

    Added Persistency

--- src/main/java/nz/ac/wgtn/swen301/a3/server/LogsServlet.java
+++ src/main/java/nz/ac/wgtn/swen301/a3/server/LogsServlet.java
@@ -1,4 +1,20 @@
 package nz.ac.wgtn.swen301.a3.server;
 
 public class LogsServlet {
+
+    public LogsServlet() {
+    }
+
+    public void doGet() {
+
+    }
+
+    public void doPost() {
+
+    }
+
+    public void doDelete() {
+
+    }
+
 }
---
+++ src/main/java/nz/ac/wgtn/swen301/a3/server/Persistency.java
@@ -0,0 +1,9 @@
+package nz.ac.wgtn.swen301.a3.server;
+
+import java.util.List;
+
+public class Persistency {
+
+    public static List DB;
+
+}
