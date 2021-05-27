aafade0030526bf9976a2178bb8a3c3330ce2cf1
Joshua <cookhjosh@ecs.vuw.ac.nz>
2021-05-28 01:48:15 +1200

    doDelete finished + start of doPost

--- src/main/java/nz/ac/wgtn/swen301/a3/server/LogsServlet.java
+++ src/main/java/nz/ac/wgtn/swen301/a3/server/LogsServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.text.ParseException;
@@ -16,6 +17,8 @@ import java.util.*;
 
 
 public class LogsServlet extends HttpServlet {
+
+    public LogsServlet(){}
 
     Map<String, Integer> priorities = Map.ofEntries(Map.entry("OFF", 0), Map.entry("FATAL", 1), Map.entry("ERROR", 2), Map.entry("WARN", 3), Map.entry("INFO", 4), Map.entry("DEBUG", 5), Map.entry("TRACE", 6), Map.entry("ALL", 7));
 
@@ -68,18 +71,40 @@ public class LogsServlet extends HttpServlet {
 
         pw.print(om.writerWithDefaultPrettyPrinter().writeValueAsString(an));
         pw.close();
+        resp.setStatus(HttpServletResponse.SC_OK);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        resp.setContentType("application/json");
+        StringBuffer string = new StringBuffer();
+        String line;
+
+        try {
+            BufferedReader br = req.getReader();
+            System.out.println(br.lines());
+            while ((line = br.readLine()) != null) {
+                string.append(line);
+            }
+        } catch (Exception e) {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "invalid input, object invalid");
+            return;
+        }
+
+        ObjectMapper map = new ObjectMapper();
+        ObjectNode node = map.createObjectNode();
+        //node.
 
         super.doPost(req, resp);
     }
 
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Persistency.DB.clear();
+        resp.setContentType("application/json");
+        resp.setStatus(HttpServletResponse.SC_OK);
 
-        super.doDelete(req, resp);
     }
 
 }
