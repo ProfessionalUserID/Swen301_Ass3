a796d4a300658574d6332b5a65a651f3883dd299
Joshua <cookhjosh@ecs.vuw.ac.nz>
2021-05-27 17:09:43 +1200

    Working towards doGet in logsservlet

--- src/main/java/nz/ac/wgtn/swen301/a3/server/LogsServlet.java
+++ src/main/java/nz/ac/wgtn/swen301/a3/server/LogsServlet.java
@@ -1,13 +1,21 @@
 package nz.ac.wgtn.swen301.a3.server;
 
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 
 public class LogsServlet extends HttpServlet {
+
+    Map<String, Integer> priorities = Map.ofEntries(Map.entry("OFF", 0), Map.entry("FATAL", 1), Map.entry("ERROR", 2), Map.entry("WARN", 3), Map.entry("INFO", 4), Map.entry("DEBUG", 5), Map.entry("TRACE", 6), Map.entry("ALL", 7));
+
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -29,7 +37,23 @@ public class LogsServlet extends HttpServlet {
 
         resp.setContentType("application/json");
 
+        ArrayList<ObjectNode> a = new ArrayList<>();
 
+        for (ObjectNode o : Persistency.DB) {
+            if (priorities.get(o.get("level").toString().toUpperCase()) <= priorities.get(m)){
+                a.add(o);
+            }
+        }
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+        a.sort((l1, l2) -> {
+            try {
+                return sdf.parse(l2.get("timestamp").asText()).compareTo(sdf.parse(l1.get("timestamp").asText()));
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            return 0;
+        });
 
         super.doGet(req, resp);
     }
--- src/main/java/nz/ac/wgtn/swen301/a3/server/Persistency.java
+++ src/main/java/nz/ac/wgtn/swen301/a3/server/Persistency.java
@@ -1,12 +1,12 @@
 package nz.ac.wgtn.swen301.a3.server;
 
-import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Persistency {
 
-    public static List<JsonNode> DB = new ArrayList<>();
+    public static List<ObjectNode> DB = new ArrayList<>();
 
 }
