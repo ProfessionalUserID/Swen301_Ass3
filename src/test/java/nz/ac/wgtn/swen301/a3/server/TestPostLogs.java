e666487a0762dc8c0592e0d0987ccc7be177fbf9
Joshua <cookhjosh@ecs.vuw.ac.nz>
2021-05-29 17:59:04 +1200

    Test6 TestPostLogs

--- src/test/java/nz/ac/wgtn/swen301/a3/server/TestPostLogs.java
+++ src/test/java/nz/ac/wgtn/swen301/a3/server/TestPostLogs.java
@@ -129,4 +129,29 @@ public class TestPostLogs {
         assertEquals(400, response.getStatus());
     }
 
+    @Test
+    public void PostLogsTest_6() throws IOException, ServletException {
+        String logEvent = "{\n" +
+                "    \"id\": \"d290f1ee-6c54-4b01-90e6-d701748f0851\",\n" +
+                "    \"message\": \"application started\",\n" +
+                "    \"timestamp\": \"04-05-2021 10:12:00\",\n" +
+                "    \"thread\": \"main\",\n" +
+                "    \"level\": \"DEBUG\",\n" +
+                "    \"errorDetails\": \"string\"\n" +
+                "  }";
+
+        Persistency.DB.clear();
+        LogsServlet servlet = new LogsServlet();
+
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        MockHttpServletResponse response = new MockHttpServletResponse();
+
+        byte[] byteArray = logEvent.getBytes();
+        request.setContent(byteArray);
+        servlet.doPost(request,response);
+        assertEquals(0, Persistency.DB.size());
+
+        assertEquals(400, response.getStatus());
+    }
+
 }
