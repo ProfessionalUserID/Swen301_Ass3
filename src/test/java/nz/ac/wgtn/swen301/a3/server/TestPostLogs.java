94069274adc3ea84f67c0b6025d5b5e5d834ca21
Joshua <cookhjosh@ecs.vuw.ac.nz>
2021-05-29 16:46:56 +1200

    Test1 TestPostLogs

--- src/test/java/nz/ac/wgtn/swen301/a3/server/TestPostLogs.java
+++ src/test/java/nz/ac/wgtn/swen301/a3/server/TestPostLogs.java
@@ -1,4 +1,29 @@
 package nz.ac.wgtn.swen301.a3.server;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import javax.servlet.ServletException;
+import java.io.IOException;
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class TestPostLogs {
+
+    @Test
+    public void PostLogsTest_1() throws IOException, ServletException {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setParameter("level","ALL");
+        request.setParameter("limit","1");
+        MockHttpServletResponse response = new MockHttpServletResponse();
+
+        LogsServlet service = new LogsServlet();
+        service.doPost(request,response);
+
+        assertTrue(Objects.requireNonNull(response.getContentType()).startsWith("application/json"));
+    }
+
 }
