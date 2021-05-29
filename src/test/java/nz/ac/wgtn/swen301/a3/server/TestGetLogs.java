36dc9dcc732fcbbb97d4ed0988b6effb1ec0e3eb
Joshua <cookhjosh@ecs.vuw.ac.nz>
2021-05-29 16:05:01 +1200

    Test1 TestGetLogs

--- src/test/java/nz/ac/wgtn/swen301/a3/server/TestGetLogs.java
+++ src/test/java/nz/ac/wgtn/swen301/a3/server/TestGetLogs.java
@@ -1,11 +1,31 @@
 package nz.ac.wgtn.swen301.a3.server;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import javax.servlet.ServletException;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestGetLogs {
 
     @Test
-    public void GetLogsTest_1(){
+    public void GetLogsTest_1() throws IOException, ServletException {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        // query parameter missing
+        LogsServlet service = new LogsServlet();
+        service.doGet(request,response);
+
+        assertEquals(400,response.getStatus());
+    }
+
+
+
+    @Test
+    public void GetLogsTest_2() throws IOException {
 
     }
 
