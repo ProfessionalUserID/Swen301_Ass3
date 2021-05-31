e736aaf69a1ebfea13e17503a5c6c8b5c4413436
Joshua <cookhjosh@ecs.vuw.ac.nz>
2021-05-31 16:57:46 +1200

    Started client

--- src/main/java/nz/ac/wgtn/swen301/a3/client/Client.java
+++ src/main/java/nz/ac/wgtn/swen301/a3/client/Client.java
@@ -1,4 +1,17 @@
 package nz.ac.wgtn.swen301.a3.client;
 
 public class Client {
+
+    static String type;
+    static String fileName;
+
+    public static void main(String[] args) {
+        if (args.length != 2){
+            System.out.println("ERROR!!");
+            return;
+        }
+        type = args[0];
+        fileName = args[1];
+    }
+
 }
