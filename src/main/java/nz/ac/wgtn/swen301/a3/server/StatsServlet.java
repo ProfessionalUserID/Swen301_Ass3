ccd654e096a2dc0bca6d63799bf6070e1b338406
Joshua <cookhjosh@ecs.vuw.ac.nz>
2021-05-31 15:33:41 +1200

    statsServlet setup

--- src/main/java/nz/ac/wgtn/swen301/a3/server/StatsServlet.java
+++ src/main/java/nz/ac/wgtn/swen301/a3/server/StatsServlet.java
@@ -1,6 +1,18 @@
 package nz.ac.wgtn.swen301.a3.server;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class StatsServlet extends HttpServlet {
+
+    public StatsServlet() {
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
 }
