d20f768bda896d9f6c59030b932951f185c2ea7b
Joshua <cookhjosh@ecs.vuw.ac.nz>
2021-05-31 16:06:02 +1200

    StatsCSVTest_1

--- src/test/java/nz/ac/wgtn/swen301/a3/server/TestStatsCSV.java
+++ src/test/java/nz/ac/wgtn/swen301/a3/server/TestStatsCSV.java
@@ -1,4 +1,27 @@
 package nz.ac.wgtn.swen301.a3.server;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import javax.servlet.ServletException;
+import java.io.IOException;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TestStatsCSV {
+
+    @Test
+    public void StatsCSVTest_1() throws ServletException, IOException {
+
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        MockHttpServletResponse response = new MockHttpServletResponse();
+
+        StatsCSVServlet service = new StatsCSVServlet();
+        service.doGet(request, response);
+
+        assertEquals(200, response.getStatus());
+    }
+
 }
